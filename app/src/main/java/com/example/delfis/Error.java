50241c71c785f9ac37f8545470ada359bede92f3
EduardoNehmeSS <eduardo.souza@germinare.org.br>
2024-09-16 07:31:16 -0300

    footer

--- app/src/main/java/com/example/delfis/Error.java
+++ app/src/main/java/com/example/delfis/Error.java
@@ -1,4 +1,23 @@
 package com.example.delfis;
 
-public class Error {
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Error extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.error);
+
+        // Botão para voltar à tela anterior
+        Button backButton = findViewById(R.id.button);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish(); // Fecha a Activity e retorna para a anterior
+            }
+        });
+    }
 }
--- app/src/main/java/com/example/delfis/Store.java
+++ app/src/main/java/com/example/delfis/Store.java
@@ -2,24 +2,42 @@ package com.example.delfis;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class Store extends AppCompatActivity {
+    BottomNavigationView nav;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.store); // Certifique-se de que este é o nome correto do layout
 
-        // Encontre o botão "home" e configure o clique
-        Button homeButton = findViewById(R.id.button3);
-        homeButton.setOnClickListener(new View.OnClickListener() {
+        // Configuração do BottomNavigationView
+        nav = findViewById(R.id.navbar);
+        nav.setSelectedItemId(R.id.lojafooter); // Seleciona o item "Loja"
+
+        // Listener para navegação entre itens do BottomNavigationView
+        nav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
-            public void onClick(View v) {
-                // Iniciar a atividade Home
-                Intent intent = new Intent(Store.this, Home.class);
-                startActivity(intent);
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Intent intent;
+
+                if (item.getItemId() == R.id.lojafooter) {
+                    // Já estamos na loja, não faça nada
+                    return true;
+                } else if (item.getItemId() == R.id.homefooter) {
+                    intent = new Intent(Store.this, Home.class);
+                } else {
+                    // Abrir a classe Error para os outros itens
+                    intent = new Intent(Store.this, Error.class);
+                }
+
+                startActivity(intent); // Inicia a nova atividade
+                return true;
             }
         });
     }
