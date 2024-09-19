1649679442f2bff6cef27f2a935c47d8492b3092
EduardoNehmeSS <eduardo.souza@germinare.org.br>
2024-09-19 11:44:06 -0300

    subindo sistema de autentificar login

---
+++ app/src/main/java/network/LoginResponse.java
@@ -0,0 +1,13 @@
+package network;
+
+public class LoginResponse {
+    private String token; // Adicione o campo do token
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
