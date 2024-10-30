4413972cd7e57aa7352730e78bb563ca94f52e3b
joaodinizaraujo <joaovictorclassroom@gmail.com>
2024-10-30 07:00:07 -0300

    subindo apk compilado e add funcionalidade dobro coins

--- app/src/main/java/goldenage/delfis/app/activity/PointsActivity.java
+++ app/src/main/java/goldenage/delfis/app/activity/PointsActivity.java
@@ -2,10 +2,12 @@ package goldenage.delfis.app.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.Button;
+import android.widget.TextView;
 
 import goldenage.delfis.app.R;
 import goldenage.delfis.app.activity.game.MathChallengesActivity;
@@ -14,13 +16,18 @@ import goldenage.delfis.app.model.response.User;
 
 public class PointsActivity extends AppCompatActivity {
     private User user;
+    private TextView textCoinsGanhadas;
 
+    @SuppressLint("DefaultLocale")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_points);
 
         user = (User) getIntent().getSerializableExtra("user");
+        textCoinsGanhadas = findViewById(R.id.textCoinsGanhadas);
+
+        textCoinsGanhadas.setText(String.format("+%d DELFICOINS", getIntent().getIntExtra("coins", 1)));
 
         new Handler().postDelayed(() -> {
             Intent intent = new Intent(PointsActivity.this, HomeActivity.class);
--- app/src/main/java/goldenage/delfis/app/util/GameUtil.java
+++ app/src/main/java/goldenage/delfis/app/util/GameUtil.java
@@ -11,22 +11,39 @@ import goldenage.delfis.app.activity.PointsActivity;
 import goldenage.delfis.app.activity.game.MathChallengesActivity;
 import goldenage.delfis.app.activity.navbar.HomeActivity;
 import goldenage.delfis.app.api.DelfisApiService;
+import goldenage.delfis.app.model.response.Powerup;
 import goldenage.delfis.app.model.response.User;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class GameUtil {
+    private static final long ID_POWERUP_DOBRO_COINS = 1L;
+
     public static void payUser(Context context, User user) {
         Map<String, Object> updates = new HashMap<>();
+        Intent intent = new Intent(context, PointsActivity.class);
 
         user.setCounterPlayedGames(user.getCounterPlayedGames() + 1);
-        if (user.getCounterPlayedGames() == 10) {
-            updates.put("level", user.getLevel() + 1);
+
+        boolean haveDoubleCoins = false;
+        for (Powerup powerup : user.getPowerups()) {
+            if (powerup.getId() == ID_POWERUP_DOBRO_COINS) {
+                haveDoubleCoins = true;
+                break;
+            }
         }
 
         updates.put("coins", user.getCoins() + 1);
+        if (haveDoubleCoins) {
+            updates.put("coins", user.getCoins() + 2);
+            intent.putExtra("coins", 2);
+        }
+        if (user.getCounterPlayedGames() == 10)
+            updates.put("level", user.getLevel() + 1);
         updates.put("points", user.getPoints() + 1);
+
+        Toast.makeText(context, "Adicionando suas recompensas...", Toast.LENGTH_LONG).show();
 
         DelfisApiService delfisApiService = RetrofitFactory.getClient().create(DelfisApiService.class);
         Call<User> call = delfisApiService.updateUserPartially(user.getToken(), user.getId(), updates);
@@ -39,6 +56,9 @@ public class GameUtil {
                         user.setCoins(updatedUser.getCoins());
                         user.setLevel(updatedUser.getLevel());
                         user.setPoints(updatedUser.getPoints());
+
+                        intent.putExtra("user", user);
+                        context.startActivity(intent);
                     }
                 } else {
                     Toast.makeText(context, "Falha ao atualizar informações.", Toast.LENGTH_LONG).show();
@@ -50,9 +70,5 @@ public class GameUtil {
                 Toast.makeText(context, "Falha na conexão.", Toast.LENGTH_LONG).show();
             }
         });
-
-        Intent intent = new Intent(context, PointsActivity.class);
-        intent.putExtra("user", user);
-        context.startActivity(intent);
     }
 }
